36e5577887e68febaa33d203d71a2442e4c90a44
not-khushal <138289978+not-khushal@users.noreply.github.com>
2025-01-14 19:46:05 +0530

    Update Devs.jsx

--- src/components/Devs.jsx
+++ src/components/Devs.jsx
@@ -15,15 +15,15 @@ import image10 from '../assets/DevsPage/image10.jpeg';
 import image11 from '../assets/DevsPage/image11.jpeg';
 
 const devsData = [
-  { id: 1, name: "Digital Agency", link: image1 },
+  { id: 1, name: "Digital Agency", link: image1,},
   { id: 2, name: "Animated Fanta", link: image2 },
   { id: 3, name: "Animated Portfolio 1", link: image3 },
   { id: 4, name: "Animated-Portfolio 2", link: image4 },
-  { id: 5, name: "Black Tumbler Cafe", link: image5 },
+  { id: 5, name: "Black Tumbler Cafe", link: image5, href: "coffee-cafe-three.vercel.app/" },
   { id: 6, name: "Shoes E-Commerce", link: image6 },
-  { id: 7, name: "Tinder For Desktop", link: image7 },
+  { id: 7, name: "Tinder For Desktop", link: image7, href: "desktop-tinder.vercel.app/"},
   { id: 8, name: "Two Good Co Clone", link: image8 },
-  { id: 9, name: "Skill X Learning", link: image9 },
+  { id: 9, name: "Skill X Learning", link: image9, href: "skill-x-ten.vercel.app/" },
   { id: 10, name: "Space Landing Page", link: image10 },
   { id: 11, name: "Tourism Landing Page", link: image11 },
 ];
@@ -69,10 +69,29 @@ const Devs = () => {
               {/* Serial number */}
               <span className="text-black font-semibold mr-8 ml-0 md:ml-16 text-xl md:text-2xl">{`0${dev.id}`}</span>
 
+
+
+
+              {/*ALSO WHERE THE LINKS WORKS SO CHANGE IN THE GITHUB AND UPLOAD*/}
+              {/*DOWN HERE*/}
+              {/*DOWN HERE*/}
+              {/*DOWN HERE*/}
+              {/*DOWN HERE*/}
               {/* Project name with hover effect */}
-              <span className="text-black font-semibold text-xl md:text-3xl group-hover:bg-black group-hover:text-white rounded-full px-6 py-2 md:px-10 md:py-2 transition-colors duration-300 cursor-pointer whitespace-nowrap inline-block">
-                {dev.name}
-              </span>
+                {dev.href ? (
+        <a
+          href={`https://${dev.href}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black font-semibold text-xl md:text-3xl group-hover:bg-black group-hover:text-white rounded-full px-6 py-2 md:px-10 md:py-2 transition-colors duration-300 cursor-pointer whitespace-nowrap inline-block"
+        >
+          {dev.name}
+        </a>
+      ) : (
+        <span className="text-black font-semibold text-xl md:text-3xl group-hover:bg-black group-hover:text-white rounded-full px-6 py-2 md:px-10 md:py-2 transition-colors duration-300 cursor-pointer whitespace-nowrap inline-block">
+          {dev.name}
+        </span>
+      )}
 
               {/* Separation line */}
               <hr className="flex-grow mt-4 border-t border-gray-300 ml-0 md:ml-10" />
